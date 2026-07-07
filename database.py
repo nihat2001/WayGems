b827fde629d41c97a72dfe35326b8159a8152685
Nihat Azizli <azizlinihat9@gmail.com>
2026-07-07 17:26:44 +0400

    Restart.

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 import redis.asyncio as redis
@@ -7,11 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST", "localhost")
+db_name = os.getenv("DB_NAME", "postgres")
 redis_host = os.getenv("REDIS_HOST", "localhost")
 
 Base = declarative_base()
 
-DATABASE_URL = f"postgresql+asyncpg://postgres:{db_password}@{db_host}:5432/postgres"
+DATABASE_URL = f"postgresql+asyncpg://postgres:{db_password}@{db_host}:5432/{db_name}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -20,6 +22,7 @@ cache = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 async def init_db():
     async with engine.begin() as conn:
+        await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
         await conn.run_sync(Base.metadata.create_all)
 
 async def get_session():
